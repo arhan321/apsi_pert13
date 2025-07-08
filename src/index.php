3f8cf84cd6868562344384a1bcb6351b544fdca0
arhan321 <arhanmcz@gmail.com>
2025-07-08 16:18:26 +0700

    freshh

--- src/index.php
+++ src/index.php
@@ -2,11 +2,21 @@
 session_start();
 require_once 'conn.php';
 
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
 
-
+$stmt = $pdo->prepare("
+SELECT blog.*, login.name AS penulis 
+FROM blog 
+JOIN 
+login ON blog.user_id = login.id 
+ORDER BY blog.created_at DSC
+");
+$stmt->execute();
+$blogs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +24,7 @@ require_once 'conn.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+     <link rel="stylesheet" href="/style/indexs.css">
 </head>
 <body>
     
