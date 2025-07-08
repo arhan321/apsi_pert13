7b2ee68747952f34d7a96bb07501b90973cbd5f7
arhan321 <arhanmcz@gmail.com>
2025-07-08 15:09:40 +0700

    frehs

---
+++ src/conn.php
@@ -0,0 +1,14 @@
+<?php 
+$host = 'db';
+$db_name = 'web_blog';
+$username = 'root';
+$password = '123';
+
+try {
+    $pdo = new PDO (" mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
+?>
