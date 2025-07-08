c108fc7e0b602a64ee8818c5a306b49cc71610e1
arhan321 <arhanmcz@gmail.com>
2025-07-08 16:20:19 +0700

    freshh

--- src/index.php
+++ src/index.php
@@ -18,6 +18,7 @@ $stmt->execute();
 $blogs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,6 +28,24 @@ $blogs = $stmt->fetchAll(PDO::FETCH_ASSOC);
      <link rel="stylesheet" href="/style/indexs.css">
 </head>
 <body>
+    <div class="header">
+    <span>Halo, <?= htmlspecialchars($_SESSION['user_name']) ?> 👋</span>
+    <a class="logout" href="logout.php">Logout</a>
+    <h2>Daftar Artikel Blog</h2>
+    <a class="btn-tambah" href="tambah_blog.php">+ Tambah Blog</a>
+</div>
+
+<?php if (count($blogs) > 0): ?>
+    <?php foreach ($blogs as $blog): ?>
+        <div class="blog">
+            <h3><?= htmlspecialchars($blog['judul']) ?></h3>
+            <p><?= nl2br(htmlspecialchars($blog['deskripsi'])) ?></p>
+            <small><i>Diposting oleh: <?= htmlspecialchars($blog['penulis']) ?> | <?= $blog['created_at'] ?></i></small>
+        </div>
+    <?php endforeach; ?>
+<?php else: ?>
+    <p>Tidak ada postingan blog untuk saat ini.</p>
+<?php endif; ?>
     
 </body>
 </html>
