cd1cccc24d056c9026ae5f47d116858567d1e715
arhan321 <arhanmcz@gmail.com>
2025-07-08 17:26:54 +0700

    freshh

--- src/tambah_blog.php
+++ src/tambah_blog.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=s, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/style/tambah_blog.css">
+    <link rel="stylesheet" href="/style/tambah_blogs.css">
 </head>
 <body>
 
